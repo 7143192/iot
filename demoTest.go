76e7f14775087b226f9d1a2db7bbb87e63efc233
7143192 <7143192@sjtu.edu.cn>
2023-06-23 00:11:57 -0700

    lyh updates params-upload demo

--- demoTest.go
+++ demoTest.go
@@ -10,29 +10,51 @@ import (
 )
 
 func ApigDemoTest(payload []byte, ctx context.RuntimeContext) (interface{}, error) {
-	var apigEvent apig.APIGTriggerEvent
-	err := json.Unmarshal(payload, &apigEvent)
-	if err != nil {
-		fmt.Println("Unmarshal failed")
-		return "invalid data", err
-	}
-	rawBody := apigEvent.GetRawBody()
-	rawBodyByte := []byte(rawBody)
-	initInfo := &defines.InitInfo{}
-	_ = json.Unmarshal(rawBodyByte, initInfo)
-	mapByte, _ := json.Marshal(initInfo.MapInfo)
-	graphByte, _ := json.Marshal(initInfo.GraphInfo)
-	ctx.GetLogger().Logf("demo rawBody:%s", rawBody)
-	ctx.GetLogger().Logf("demo mapInfo:%s", string(mapByte))
-	ctx.GetLogger().Logf("demo rawBody:%s", string(graphByte))
-	// ctx.GetLogger().Logf("payload:%s", apigEvent.String())
+	//var apigEvent apig.APIGTriggerEvent
+	//err := json.Unmarshal(payload, &apigEvent)
+	//if err != nil {
+	//	fmt.Println("Unmarshal failed")
+	//	return "invalid data", err
+	//}
+	//rawBody := apigEvent.GetRawBody()
+	//rawBodyByte := []byte(rawBody)
+	//initInfo := &defines.InitInfo{}
+	//_ = json.Unmarshal(rawBodyByte, initInfo)
+	//mapByte, _ := json.Marshal(initInfo.MapInfo)
+	//graphByte, _ := json.Marshal(initInfo.GraphInfo)
+	//ctx.GetLogger().Logf("demo rawBody:%s", rawBody)
+	//ctx.GetLogger().Logf("demo mapInfo:%s", string(mapByte))
+	//ctx.GetLogger().Logf("demo rawBody:%s", string(graphByte))
+	//// ctx.GetLogger().Logf("payload:%s", apigEvent.String())
+	//apigResp := apig.APIGTriggerResponse{
+	//	Body: apigEvent.String(),
+	//	Headers: map[string]string{
+	//		"content-type": "application/json",
+	//	},
+	//	StatusCode: 200,
+	//}
+	//return apigResp, nil
+
+	fmt.Printf("demo got payload = %v\n", string(payload))
+	gotMap := make(map[string]string)
+	input := defines.InputInfo{}
+	_ = json.Unmarshal(payload, &gotMap)
+	inputStr := gotMap["input"]
+	_ = json.Unmarshal([]byte(inputStr), &input)
+	mapInfo := input.Init.MapInfo
+	mapByte, _ := json.Marshal(mapInfo)
+	graphInfo := input.Init.GraphInfo
+	graphByte, _ := json.Marshal(graphInfo)
+	fmt.Printf("demo got mapInfo = %v\n", string(mapByte))
+	fmt.Printf("demo got graphInfo = %v\n", string(graphByte))
 	apigResp := apig.APIGTriggerResponse{
-		Body: apigEvent.String(),
+		Body: string(payload),
 		Headers: map[string]string{
 			"content-type": "application/json",
 		},
 		StatusCode: 200,
 	}
+
 	return apigResp, nil
 }
 
--- redisTest.go
+++ redisTest.go
@@ -3,69 +3,59 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"huaweicloud.com/go-runtime/events/apig"
 	"huaweicloud.com/go-runtime/go-api/context"
 	"huaweicloud.com/go-runtime/pkg/runtime"
 	"iot/pkg/defines"
 )
 
 func ApigRedisTest(payload []byte, ctx context.RuntimeContext) (interface{}, error) {
-	var apigEvent apig.APIGTriggerEvent
-	err := json.Unmarshal(payload, &apigEvent)
-	if err != nil {
-		fmt.Println("Unmarshal failed")
-		return "invalid data", err
-	}
+	//var apigEvent apig.APIGTriggerEvent
+	//err := json.Unmarshal(payload, &apigEvent)
+	//if err != nil {
+	//	fmt.Println("Unmarshal failed")
+	//	return "invalid data", err
+	//}
+	//
+	//rawBody := apigEvent.GetRawBody()
+	//rawBodyByte := []byte(rawBody)
+	//initInfo := &defines.InitInfo{}
+	//_ = json.Unmarshal(rawBodyByte, initInfo)
+	//mapByte, _ := json.Marshal(initInfo.MapInfo)
+	//graphByte, _ := json.Marshal(initInfo.GraphInfo)
+	//ctx.GetLogger().Logf("userData:%s", apigEvent.UserData)
+	//ctx.GetLogger().Logf("rawBody:%s", rawBody)
+	//ctx.GetLogger().Logf("mapInfo:%s", string(mapByte))
+	//ctx.GetLogger().Logf("graphInfo:%s", string(graphByte))
+	//apigResp := apig.APIGTriggerResponse{
+	//	// Body: apigEvent.String(),
+	//	Body: rawBody,
+	//	Headers: map[string]string{
+	//		"content-type": "application/json",
+	//	},
+	//	StatusCode: 200,
+	//}
 
-	rawBody := apigEvent.GetRawBody()
-	rawBodyByte := []byte(rawBody)
-	initInfo := &defines.InitInfo{}
-	_ = json.Unmarshal(rawBodyByte, initInfo)
-	mapByte, _ := json.Marshal(initInfo.MapInfo)
-	graphByte, _ := json.Marshal(initInfo.GraphInfo)
-	ctx.GetLogger().Logf("userData:%s", apigEvent.UserData)
-	ctx.GetLogger().Logf("rawBody:%s", rawBody)
-	ctx.GetLogger().Logf("mapInfo:%s", string(mapByte))
-	ctx.GetLogger().Logf("graphInfo:%s", string(graphByte))
-	//// then store these info into Redis.
-	//rd := redis.NewClient(&redis.Options{
-	//	Addr:     "192.168.189.129:6379", // url
-	//	Password: "",
-	//	DB:       0, // No.0 DB
-	//})
-	//err = rd.Set("mapKey", string(mapByte), 0).Err()
-	//if err != nil {
-	//	ctx.GetLogger().Logf("set mapInfo error msg = %s", err)
-	//}
-	//err = rd.Set("graphKey", string(graphByte), 0).Err()
-	//if err != nil {
-	//	ctx.GetLogger().Logf("set graphInfo error msg = %s", err)
-	//}
-	//val, err := rd.Get("mapKey").Result()
-	//if err != nil {
-	//	ctx.GetLogger().Logf("get mapInfo error msg = %s", err)
-	//} else {
-	//	ctx.GetLogger().Logf("get mapInfo = %s", val)
-	//}
-	//val1, err := rd.Get("graphKey").Result()
-	//if err != nil {
-	//	ctx.GetLogger().Logf("get graphInfo error msg = %s", err)
-	//} else {
-	//	ctx.GetLogger().Logf("get graphInfo = %s", val1)
-	//}
-	apigResp := apig.APIGTriggerResponse{
-		// Body: apigEvent.String(),
-		Body: rawBody,
-		Headers: map[string]string{
-			"content-type": "application/json",
-		},
-		StatusCode: 200,
-	}
 	//// call the demo function here.
 	//url := "http://05757d8afcd54fc6881df55c8b2e2908.apig.cn-east-3.huaweicloudapis.com/demo_test"
 	//res, err := http.Post(url, "application/json", strings.NewReader(rawBody))
 	//defer res.Body.Close()
-	return apigResp, nil
+
+	input := defines.InputInfo{}
+	_ = json.Unmarshal(payload, &input)
+	fmt.Printf("got input info = %v\n", string(payload))
+	//apigResp := apig.APIGTriggerResponse{
+	//	// Body: apigEvent.String(),
+	//	Body: string(payload),
+	//	Headers: map[string]string{
+	//		"content-type": "application/json",
+	//	},
+	//	StatusCode: 200,
+	//}
+	apigResp := map[string]interface{}{
+		"result": string(payload),
+	}
+	apigRespByte, _ := json.Marshal(&apigResp)
+	return string(apigRespByte), nil
 }
 
 func main() {
